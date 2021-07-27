11baf62aad73b7c43e032d938e1540073506e42f
nitachang <80136827+nitachang@users.noreply.github.com>
2021-07-27 11:37:29 +0800

    Add files via upload

---
+++ lesson6.py
@@ -0,0 +1,27 @@
+score=[100,69,84,40,98,91,63]
+max=100
+for x in score:
+    if max>x:
+        max=x
+print('max:',max,'x:',x)
+print('max:',max)
+   
+
+print('---------------')
+
+score=[100,69,84,40,98,91,63]
+max2=0
+for z in score:
+    if max2<z:
+        max2=z
+print('max2:',max2,'z:',z)
+print('max2:',max2)
+
+print('---------------')
+
+s=[20,30,49,89]
+y=0
+z=0
+while y<4:
+    print(score[y])
+    y=y+1
